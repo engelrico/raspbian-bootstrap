bb73af4ac082b10b51a6b519f11c4a9f8813ec02
engelrico <rico@engelrico.de>
2020-12-12 13:41:10 +0000

    moved logScripts to role weather

--- roles/weather/script/cpuTemp.py
+++ roles/weather/script/cpuTemp.py
@@ -4,24 +4,33 @@
 
 import re, commands
 import requests
+import socket
+import os
+
+cmd_shell  = 'vcgencmd measure_temp'
+host       = socket.gethostname()
+influx_url = 'http://192.168.188.29:8086'
+influx_db  = 'home'
 
 
-def check_CPU_temp():
-    temp = None
-    err, msg = commands.getstatusoutput('vcgencmd measure_temp')
-    if not err:
-        m = re.search(r'-?\d\.?\d*', msg)   # https://stackoverflow.com/a/49563120/3904031
-        try:
-            temp = float(m.group())
-        except:
-            pass
-    return temp, msg
+def getCPUtemperature():
+    res = os.popen(cmd_shell).readline()
+    return(res.replace("temp=","").replace("'C\n",""))
 
-temp, msg = check_CPU_temp()
+# Return % of CPU used by user as a character string                                
+def getCPUuse():
+    return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()))
 
-data = 'garden_cpu_temp,host=dobby,location=roaming value=' + str(temp)
-#print data
-output = requests.post('http://192.168.188.29:8086/write?db=home', data=data)
+# log to influxdb
+def logData(key,value):
+  data = key+',host='+str(host)+',location=roaming value=' + value
+  #print data
+  output = requests.post(influx_url+'/write?db='+influx_db, data=data)
+  
 
-#print "temperature (" + u'\xb0' + "C): ", temp
-#print "full message:    ", msg
+  
+temp  = getCPUtemperature()
+logData(str(host)+'_cpu_temp',temp)
+
+cpu  = getCPUuse()
+logData(str(host)+'_cpu_use',cpu)
--- roles/weather/script/weatherLog.py
+++ roles/weather/script/weatherLog.py
@@ -23,6 +23,11 @@
 import Adafruit_DHT
 import requests
 import time
+import socket
+
+host       = socket.gethostname()
+influx_url = 'http://192.168.188.29:8086'
+influx_db  = 'home'
 
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
@@ -44,10 +49,10 @@ timestamp = time.time()
 
 if humidity is not None and temperature is not None:
 	#Write the data to the database
-	data = 'garden_temp,host=dobby,location=roaming value=' + str(temperature)
-	output = requests.post('http://192.168.188.29:8086/write?db=home', data=data)
-	data = 'garden_humidity,host=dobby,location=roaming value=' + str(humidity)
-	output = requests.post('http://192.168.188.29:8086/write?db=home', data=data)
+	data = 'garden_temp,host='+str(host)+',location=roaming value=' + str(temperature)
+	output = requests.post(influx_url+'/write?db='+influx_db, data=data)
+	data = 'garden_humidity,host='+str(host)+',location=roaming value=' + str(humidity)
+	output = requests.post(influx_url+'/write?db='+influx_db, data=data)
 	
 else:
 	print 'Failed to get reading. Try again!'#!/usr/bin/python
